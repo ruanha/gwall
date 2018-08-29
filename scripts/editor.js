7a07a8fba90aa3563cd3cc49fe8ddbd334f852c2
ruanha <rune_hartvig@hotmail.com>
2018-08-29 11:30:30 +0200

    mouse wall-menu closes again

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,12 +1,22 @@
 "use strict"
 
+/* MOUSE CLICK WALL MENU */
 const wall = document.getElementById("wall")
 
 wall.addEventListener('click', (e)=>{
-	console.log(e)
-
 	let mouseMenu = document.getElementById("editor-wall-menu")
-	mouseMenu.style.visibility = "visible"
-	mouseMenu.style.top = e.layerY+"px"
-	mouseMenu.style.left = e.layerX+"px"
+	if (mouseMenu.style.visibility === "hidden" ){
+		mouseMenu.style.visibility = "visible"
+		mouseMenu.style.top = e.layerY+"px"
+		mouseMenu.style.left = e.layerX+"px"		
+	}
+	else{
+		mouseMenu.style.visibility = "hidden"
+	}
 })
+
+/* MENU EVENTS */
+/* STOCK */
+
+
+/* 10x10 FRAME */
