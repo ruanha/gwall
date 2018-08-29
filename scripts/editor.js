5819b527029f554768a9d2a2d2a3515f2e328b22
ruanha <rune_hartvig@hotmail.com>
2018-08-29 11:21:59 +0200

    adds wall mouse click menu

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,1 +1,12 @@
 "use strict"
+
+const wall = document.getElementById("wall")
+
+wall.addEventListener('click', (e)=>{
+	console.log(e)
+
+	let mouseMenu = document.getElementById("editor-wall-menu")
+	mouseMenu.style.visibility = "visible"
+	mouseMenu.style.top = e.layerY+"px"
+	mouseMenu.style.left = e.layerX+"px"
+})
