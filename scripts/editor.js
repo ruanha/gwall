7124e41d138704648906842d2a470fab8790bc26
ruanha <rune_hartvig@hotmail.com>
2018-08-30 12:30:11 +0200

    refactors mouse-menu

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,7 +1,23 @@
 "use strict"
 
-let user = {
-	floatingFrame:false,
+let activeMenu = {
+	bool:false,
+	id:"",
+	obj:{},
+	reset(){
+		this.bool = false
+		this.id = ""
+		this.obj = {}
+	},
+	set(id, obj){
+		this.bool = true
+		this.id = id
+		this.obj = obj
+	},
+	removeFromDOM(){
+		let domMenuToRemove = document.getElementById(this.id)
+		domMenuToRemove.parentNode.removeChild(domMenuToRemove)
+	}
 }
 
 let activeFrame = {
@@ -13,127 +29,45 @@ let activeFrame = {
 	color:"black",
 	floats:false,
 }
-
-let wallEvents = []
-/* WALL EVENTS:
-click events:
-floatingFrame active --> place frame
-else if (wall):
-open wall mouse menu
-else if (frame)
-open frame menu
-else if (print)
-open print menu
-*/
-
-
-const wall = document.getElementById("wall")
-wall.addEventListener('click', (e)=>{
-	if ( e.target === wall || e.target === document.getElementById("background") ){
-		// if wall
-		if ( activeFrame.floats ){
-			placeFrame(e)
+// LISTEN FOR CLICK EVENT EVERYWHERE
+document.addEventListener('click',(e)=>{
+	console.log("something clicked...")
+	if ( activeMenu.bool && (e.target.className !== "mouse-menu-item" && 
+				e.target.className !== "mouse-menu-header") ){
+		console.log("class name: " + e.target.className)
+		//there is an active menu, you didn't click on it, i'm closing it. make up your damn mind!
+		console.log("closing active menu")
+		activeMenu.removeFromDOM()
+		activeMenu.reset()
+	}
+	else if ( !activeMenu.bool) {
+		// okay there is no active menu. Should there be one?
+		if ( e.target.id === "wall" || e.target.id === "background" ){
+			if ( activeFrame.floats ){
+				// place a frame?
+				console.log("placing frame")
+				placeFrame(e)
+			}
+			else if ( e.target.className === "placed-frame" ){
+				console.log("wall print menu")
+				wallPrintMenu(e)
+			}
+			else {
+				// open wall menu
+				console.log("wall mouse menu")
+				wallMouseMenu(e)
+			}
 		}
-		else{
-			wallMouseMenu(e)
-		}
 	}
-	else if ( e.target.className === "placed-frame" ){
-		wallPrintMenu()
+	else if ( activeMenu.bool && e.target.className === "mouse-menu-item" ) {
+		//there is an active menu and you clicked on an active field in it, good job!
+		console.log("menu item clicked")
+		// 1) find which was clicked, 2) find its associated function in activeMenu.events
+		let callFunction = activeMenu.obj.events[e.target.innerHTML][0]
+		let variables = activeMenu.obj.events[e.target.innerHTML].slice(1)
+		callFunction(...variables)
 	}
-})
-
-/* MENU EVENTS */
-/* STOCK */
-let choiceStock = document.getElementById("choice-stock")
-choiceStock.addEventListener('click', ()=>{
-	//close menu
-	let mouseMenu = document.getElementById("editor-wall-menu")
-	mouseMenu.style.visibility = "hidden"
-	//open stock background menu
-	let menuBackground = document.getElementById("menu-background")
-	menuBackground.style.visibility = "visible"
-})
-
-let chooseBackgroundButtons = document.getElementsByClassName("menu-choice-background-image")
-for (let i=0; i<chooseBackgroundButtons.length; i++){
-	let button = chooseBackgroundButtons[i]
-	let background = document.getElementById("background")
-	button.addEventListener('click', ()=>{
-		background.src = button.attributes.src.value
-		button.parentNode.parentNode.style.visibility = "hidden"
-
-	})
-}
-
-/*INSERT FRAMES (HELPER FUNCTIONS CLASS) */
-class Frame{
-	constructor(width, height){
-		this.width = width
-		this.height = height
-		this.portrait = true
-		this.top = 0
-		this.left = 0
-		this.color = "black"
-		this.border = 2
-		this.floating = false
+	else {
+		console.log("i have no idea how you did that...")
 	}
-}
-
-function setFloatingFrame(activeFrame, e){
-	let floatingFrame = document.createElement("div")
-	floatingFrame.setAttribute("class", "floating-frame")
-	floatingFrame.setAttribute("id", "floating-frame")
-	floatingFrame.style.top = e.layerY+"px"
-	floatingFrame.style.left = e.layerX+"px"
-	floatingFrame.style.width = activeFrame.width+"px"
-	floatingFrame.style.height = activeFrame.height+"px"
-	floatingFrame.style.border = "1px solid black"
-	return floatingFrame
-}
-
-
-/* 15x20 FRAME */
-let choice15x20 = document.getElementById("choice-15x20")
-choice15x20.addEventListener('click', (e)=>{
-	let frame = new Frame (60, 80)
-	Object.assign(activeFrame, frame)
-	activeFrame.floats = true
-
-	let mouseMenu = document.getElementById("editor-wall-menu")
-	mouseMenu.style.visibility = "hidden"
-	//make frame div
-	wall.appendChild(setFloatingFrame(activeFrame, e))
-	//ADD MOUSE MOVE EVENT TO WALL
-	wall.addEventListener('mousemove', onMouseMove)
-})
-
-/* 20x30 FRAME */
-let choice20x30 = document.getElementById("choice-20x30")
-choice20x30.addEventListener('click', (e)=>{
-	let frame = new Frame (80, 120)
-	Object.assign(activeFrame, frame)
-	activeFrame.floats = true
-
-	let mouseMenu = document.getElementById("editor-wall-menu")
-	mouseMenu.style.visibility = "hidden"
-	//make frame div
-	wall.appendChild(setFloatingFrame(activeFrame, e))
-	//ADD MOUSE MOVE EVENT TO WALL
-	wall.addEventListener('mousemove', onMouseMove)
-})
-
-/* 30x40 FRAME */
-let choice30x40 = document.getElementById("choice-30x40")
-choice30x40.addEventListener('click', (e)=>{
-	let frame = new Frame (120, 160)
-	Object.assign(activeFrame, frame)
-	activeFrame.floats = true
-
-	let mouseMenu = document.getElementById("editor-wall-menu")
-	mouseMenu.style.visibility = "hidden"
-	//make frame div
-	wall.appendChild(setFloatingFrame(activeFrame, e))
-	//ADD MOUSE MOVE EVENT TO WALL
-	wall.addEventListener('mousemove', onMouseMove)
 })
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,13 +1,19 @@
+"use strict"
+
 function wallMouseMenu(e){
-	let mouseMenu = document.getElementById("editor-wall-menu")
-	if (mouseMenu.style.visibility === "hidden" ){
-		mouseMenu.style.visibility = "visible"
-		mouseMenu.style.top = e.layerY+"px"
-		mouseMenu.style.left = e.layerX+"px"		
+	let choices = { "insert background":["stock-photo", "upload"],
+				"insert frame":["15x20", "20x30", "30x40"]
 	}
-	else{
-		mouseMenu.style.visibility = "hidden"
-	}
+	let events = { "stock-photo":[choiceStock], "upload":[nothing], "15x20":[frameChoice, 60, 80], 
+			"20x30":[frameChoice, 80, 120], "30x40":[frameChoice, 120, 160] }
+
+	let wallMenu = new MouseMenu("editor-wall-menu", choices, events)
+	activeMenu.set("editor-wall-menu", wallMenu)
+	wallMenu.insertMenu(e.layerY, e.layerX)
+}
+
+function nothing(){
+
 }
 
 function onMouseMove(e){
@@ -54,7 +60,8 @@ function renderFrame(){
 function wallPrintMenu(e){
 	//frame menu: delete, change color, border size
 	// arrow key control
-	
+	let frame = document.getElementsByClassName("placed-frame")	
+	photo.insertMenu(frame[0].offsetTop + e.layerY, frame[0].offsetLeft + e.layerX)
 
 }
 
@@ -65,4 +72,101 @@ function clickFrame(e){
 
 function dragComposition(e){
 	//onmousedown move all frames
+}
+
+/*INSERT FRAMES (HELPER FUNCTIONS CLASS) */
+class Frame{
+	constructor(width, height){
+		this.width = width
+		this.height = height
+		this.portrait = true
+		this.top = 0
+		this.left = 0
+		this.color = "black"
+		this.border = 2
+		this.floating = false
+	}
+}
+
+function setFloatingFrame(activeFrame){
+	let floatingFrame = document.createElement("div")
+	let wall = document.getElementById("wall")
+	floatingFrame.setAttribute("class", "floating-frame")
+	floatingFrame.setAttribute("id", "floating-frame")
+	floatingFrame.style.top = wall.layerY+"px"
+	floatingFrame.style.left = wall.layerX+"px"
+	floatingFrame.style.width = activeFrame.width+"px"
+	floatingFrame.style.height = activeFrame.height+"px"
+	floatingFrame.style.border = "1px solid black"
+	return floatingFrame
+}
+
+class MouseMenu{
+	constructor(id, choices, events){
+		this.choices = choices
+		this.events = events
+		this.id = id
+	}
+	renderHTML(){
+		let html = ""
+		//html += '<div class="mouse-menu" id="editor-'+this.name+'-menu">'
+		for ( let header in this.choices ){
+			html += '<div class="mouse-menu-header">'+header+'</div>'
+			for ( let i=0; i< this.choices[header].length; i++ ){
+				let name = this.choices[header][i]
+				html += '<div class="mouse-menu-item" name="'+name+'" id="choice-'+name+'">'+name+'</div>'
+			}
+		}
+		//html += '</div>'
+		return html
+	}
+	insertMenu(top, left){
+		console.log(top, left)
+		let wall = document.getElementById("wall")
+		let menu = document.createElement("div")
+		menu.setAttribute("id", this.id)
+		menu.setAttribute("class", "mouse-menu")
+		menu.innerHTML = this.renderHTML()
+		menu.style.top = top+"px"
+		menu.style.left = left+"px"
+		menu.style.visibility = "visible"
+		wall.appendChild(menu)
+	}
+}
+
+function frameChoice(width, height){
+	/* set the event that fires when a frame is selected
+	mouse menu.
+	*/
+	let frame = new Frame (width, height)
+	Object.assign(activeFrame, frame)
+	activeFrame.floats = true
+
+	activeMenu.removeFromDOM()
+	activeMenu.reset()
+	//make frame div
+	wall.appendChild(setFloatingFrame(activeFrame))
+	//ADD MOUSE MOVE EVENT TO WALL
+	wall.addEventListener('mousemove', onMouseMove)
+}
+function choiceStock(){
+	//close menu
+	let mouseMenu = document.getElementById("editor-wall-menu")
+	//open stock background menu
+	let menuBackground = document.getElementById("menu-background")
+	menuBackground.style.visibility = "visible"
+}
+
+
+// IF REFACTOR: remove the id="menu-background" div element
+// refactor this function to create the menu on click (use MouseMenu function? not sure its poss.)
+// detect click with the add.eventListener on doucment in editor.js
+let chooseBackgroundButtons = document.getElementsByClassName("menu-choice-background-image")
+for (let i=0; i<chooseBackgroundButtons.length; i++){
+	let button = chooseBackgroundButtons[i]
+	let background = document.getElementById("background")
+	button.addEventListener('click', ()=>{
+		background.src = button.attributes.src.value
+		button.parentNode.parentNode.style.visibility = "hidden"
+	})
 }
